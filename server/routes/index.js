70c46e469d40f4860a125081654769e5feebab27
Karim H Kyler <karim.kyler@gmail.com>
2015-01-22 16:23:25 -0800

    add OAuth for fitbit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var FitbitStrategy = require('passport-fitbit').Strategy;
+var keys = require('../keys.js');
+
+var url = 'http://localhost:1337/auth/fitbit/callback';
+
+passport.use(new FitbitStrategy({
+  consumerKey: keys.consumerKey,
+  consumerSecret: keys.consumerSecret,
+  callbackURL: url
+},
+function (token, tokenSecret, profile, done) {
+  console.log('token: ', token);
+  console.log('tokenSecret', tokenSecret);
+  console.log('profile', profile);
+  console.log('done:', done);
+}
+    
+));
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('rendering homepage');
+router.get('/auth/fitbit/callback', passport.authenticate('fitbit', { failureRedirect: '/login' }), function (req, res, next) {
+
+  console.log('req: ', req);
   res.render('index', { title: 'Express' });
 });
 
