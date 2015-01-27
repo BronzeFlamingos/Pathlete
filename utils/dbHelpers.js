41607d1abe6ef5c0b791e05d4ff4a0290fb8e234
Karim H Kyler <karim.kyler@gmail.com>
2015-01-26 16:25:22 -0800

    feat deploy
    deployable as well as runnable on your localhost

--- utils/dbHelpers.js
+++ utils/dbHelpers.js
@@ -6,18 +6,19 @@ module.exports = {
     var err = '';
     db.child('users').child(profile.id).once('value', function (data) {
       if (data.val() === null){
-        err = 'user not in DB!';
+        var user = {};
+        //user.id = profile.id;
+        user.tokenSecret = tokenSecret;
+        user.token = token;
+        user.name = profile._json.user.fullName;
+        user.strideRunning = profile._json.user.strideLengthRunning;
+        user.strideWalking = profile._json.user.strideLengthWalking;
+        user.units = profile._json.user.distanceUnit;
+        db.child('users').child(profile.id).set(user);
+      } else {
+        db.child('users').child(profile.id).update({tokenSecret: tokenSecret, token: token});
       }
-      var user = {};
-      user.id = profile.id;
-      user.tokenSecret = tokenSecret;
-      user.token = token;
-      user.name = profile._json.user.fullName;
-      user.strideRunning = profile._json.user.strideLengthRunning;
-      user.strideWalking = profile._json.user.strideLengthWalking;
-      user.units = profile._json.user.distanceUnit;
-      db.child('users').push(user);
-      console.log('THIS IS USER', user);
+
       done(err, profile._json.user);
       
     });
