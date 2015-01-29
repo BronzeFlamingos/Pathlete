a3f20a0ade21446cae80c817db198c857d414f35
sampsep <saminsepasi@gmail.com>
2015-01-28 18:01:25 -0800

    (feat) working DB query

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports ={
   extend: function (target) {
-    var args = arguments.slice();
+    var args = Array.prototype.slice.call(arguments);
     for (var i = 0; i < args.length; i++){
       for (var key in args[i]){
         target[key] = args[i][key];
