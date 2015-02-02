f01c2a5ff7fa8672e8da9023cac78fba5fc4b7ab
sampsep <saminsepasi@gmail.com>
2015-02-02 12:30:59 -0800

    (feat) working on documentation

--- utils/fitbit.js
+++ utils/fitbit.js
@@ -8,21 +8,24 @@ module.exports = exports = {
       consumerKey: process.env.CONSUMER_KEY,
       consumerSecret: process.env.CONSUMER_SECRET,
       callbackURL: '/auth/fitbit/callback',
+    }, function (token, tokenSecret, profile, done) {   
+      //after oath login call this success handler
       userAuthorizationURL:'https://www.fitbit.com/oauth/authorize'
-    }, function (token, tokenSecret, profile, done) {   //after oath login call this success handler
-          console.log(profile); //returns user profile 
-          dbHelper.addUser(token, tokenSecret, profile);  //add user to db
-
-          exports.getStats(profile.id, token, tokenSecret).then(function() { //this line waits for 26 to finish
-            done(null, profile._json.user); //done tells the program you are done and want to go to the next step
+          //add user to db
+          dbHelper.addUser(token, tokenSecret, profile);  
+          //this line waits for 26 to finish
+          exports.getStats(profile.id, token, tokenSecret).then(function() { 
+            //done tells the program you are done and want to go to the next step
+            done(null, profile._json.user); 
           });
-          
         }),
 
   getStats: function (userID, token, secret) {
     var client = new FitbitApiClient(process.env.CONSUMER_KEY, process.env.CONSUMER_SECRET);
-    return client.requestResource('/activities.json', 'GET', token, secret).then(function (data) {  //creates the request to get activites json from fitbit
-        dbHelper.addUserStats(userID, data[0]); //success handler for req, return the promise
+    //creates the request to get activites json from fitbit
+    return client.requestResource('/activities.json', 'GET', token, secret).then(function (data) {  
+        //success handler for req, return the promise
+        dbHelper.addUserStats(userID, data[0]); 
       }, function (err) {
         console.log('ERROR!',err);
       });
